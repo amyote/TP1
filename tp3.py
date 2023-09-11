51d9e89c1c992d261714edbc0a60f560572fe730
amyote <144702416+amyote@users.noreply.github.com>
2023-09-11 10:43:39 -0400

    Les autres fichiers, possiblement incomplets

---
+++ tp3.py
@@ -0,0 +1,10 @@
+import random
+
+
+niveauVie = 20
+
+forceMin = 1
+forceMax = 6
+
+forceAdversaire = random.randint(forceMin, forceMax)
+print(f"Vous tombez face a face avec un adversaire de difficulte: {forceAdversaire}.")
