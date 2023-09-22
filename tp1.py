439d5500c61c973c1457c25e2f234aa990dd8345
amyote <144702416+amyote@users.noreply.github.com>
2023-09-22 10:10:38 -0400

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,6 +1,5 @@
-val = input("Entrez votre chaine de caracteres: ")
+def word_count():
+    val = input("Entrez votre chaine de caracteres: ")
+    print("Il y a " + str(val.count(" ") + 1) + " mots dans ce que vous avez dit.")
 
-def word_count(val):
-    return val.count(" ") + 1
-
-print("Il y a " + str(word_count(val)) + " mots dans ce que vous avez dit.")
+word_count()
